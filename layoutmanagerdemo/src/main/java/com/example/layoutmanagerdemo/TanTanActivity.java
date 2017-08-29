35dfae930af4b7b5f0fb85f72a2313ec5acd5400
RainFool <rainfool1024@gmail.com>
2017-08-29 19:05:00 +0800

    项目搭建完成

---
+++ layoutmanagerdemo/src/main/java/com/example/layoutmanagerdemo/TanTanActivity.java
@@ -0,0 +1,13 @@
+package com.example.layoutmanagerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TanTanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tan_tan);
+    }
+}
